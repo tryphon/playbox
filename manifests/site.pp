614af769f4a4b3ad7dc45714e4cf88dbb99a1786
Alban Peignier <alban@tryphon.eu>
2010-03-01 22:57:20 +0100

    Update module box with network configuration

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,21 +4,13 @@ import "classes/*"
 
 $source_base="/tmp/puppet"
 
-file { "/etc/network/interfaces": 
-   content => "auto lo
-iface lo inet loopback
-
-auto eth0
-iface eth0 inet dhcp
-"
-}
+$box_name="playbox"
 
 # TODO replace by puppet boot
 include readonly::initvarlog
 
-include network::base
-include network::dhcp::readonly
-include network::ifplugd
+include network
+include network::interfaces::deprecated
 
 include alsa::common
 include liquidsoap::readonly
