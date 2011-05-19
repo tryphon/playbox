01faca4af55a8e6550a6ff44291120b3568fa677
Alban Peignier <alban@tryphon.eu>
2011-05-19 19:21:14 +0200

    Use /boot/release.tar to download new release. Refs #3

--- manifests/site.pp
+++ manifests/site.pp
@@ -21,5 +21,7 @@ include volumepad::readonly
 
 include rsync::cron
 
-$release_cron_before_download="mount -o remount,size=80% /tmp"
+$release_cron_download_directory="/boot"
+$release_cron_before_download="mount -o remount,rw /boot"
+$release_cron_after_download="mount -o remount,ro /boot"
 include release::cron
